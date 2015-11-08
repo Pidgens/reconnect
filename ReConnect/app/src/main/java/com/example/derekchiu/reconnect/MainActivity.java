742c3323a635411c046b248d670fcdd3d2d3c297
pidgens <dederekchiu@berkeley.edu>
2015-11-07 20:44:43 -0800

    fb login

--- ReConnect/app/src/main/java/com/example/derekchiu/reconnect/MainActivity.java
+++ ReConnect/app/src/main/java/com/example/derekchiu/reconnect/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.derekchiu.reconnect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        info = (TextView) findViewById(R.id.info);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: " +
+                        loginResult.getAccessToken().getUserId() +
+                        "\n " +
+                        "Auth Token: " +
+                        loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login Attempted Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                info.setText("Login Attempt failed.");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
+
